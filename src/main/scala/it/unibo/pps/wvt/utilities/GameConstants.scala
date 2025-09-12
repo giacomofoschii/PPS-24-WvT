9c64be9953e98699b6598fcd076114905858225e
Giovanni <giovanni.pisoni@studio.unibo.it>
2025-09-12 18:41:56 +0200

    [ENGINE] Implemented EventHandlers to handle all the event in the GameEngine core. WITH ERROR, TO RESOLVE

--- src/main/scala/it/unibo/pps/wvt/utilities/GameConstants.scala
+++ src/main/scala/it/unibo/pps/wvt/utilities/GameConstants.scala
@@ -1,6 +1,18 @@
 package it.unibo.pps.wvt.utilities
 
 object GameConstants {
+  //Grid dimensions
   val GRID_ROWS: Int = 5
   val GRID_COLS: Int = 9
+  // Elixir generation parameters
+  val INITIAL_ELIXIR: Int = 200
+  // Troll parameters
+  val BASE_TROLL_HEALTH: Int = 100
+  val WARRIOR_TROLL_HEALTH: Int = 200
+  val ASSASSIN_TROLL_HEALTH: Int = 50
+  val THROWER_TROLL_HEALTH: Int = 75
+  val BASE_TROLL_REWARD: Int = 25
+  val WARRIOR_TROLL_REWARD: Int = 75
+  val ASSASSIN_TROLL_REWARD: Int = 100
+  val THROWER_TROLL_REWARD: Int = 125
 }
