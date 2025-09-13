5063d38486d15d87afd2c5707e39676158979375
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-09-13 15:30:42 +0200

    [VIEW] Added main game window

---
+++ src/main/scala/it/unibo/pps/wvt/view/GameView.scala
@@ -0,0 +1,19 @@
+package it.unibo.pps.wvt.view
+
+import it.unibo.pps.wvt.utilities.ViewConstants.GAME_MAP_SCALE_FACTOR
+import scalafx.scene.Parent
+import scalafx.scene.image.ImageView
+import it.unibo.pps.wvt.view.ButtonFactory.*
+import it.unibo.pps.wvt.view.ImageFactory.*
+import scalafx.scene.layout.StackPane
+
+object GameView {
+  def apply(): Parent = {
+    lazy val backgroundImage = 
+      createBackgroundView("/background_grid.png", GAME_MAP_SCALE_FACTOR).getOrElse(new ImageView())
+
+    new StackPane {
+      children = Seq(backgroundImage)
+    }
+  }
+}
