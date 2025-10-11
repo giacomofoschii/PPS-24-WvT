aabc10e8458ced98827d26787b7ab9628bf85b18
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-10-11 16:39:28 +0200

    [SPRINT4] [REFACTOR] ImageFactory refactored

--- src/main/scala/it/unibo/pps/wvt/view/ImageFactory.scala
+++ src/main/scala/it/unibo/pps/wvt/view/ImageFactory.scala
@@ -2,36 +2,62 @@ package it.unibo.pps.wvt.view
 
 import scalafx.scene.image.{Image, ImageView}
 import scala.collection.mutable
+import java.io.InputStream
 
-object ImageFactory {
+object ImageFactory:
 
   private val imageCache: mutable.Map[String, Image] = mutable.Map.empty
 
   def createBackgroundView(path: String, factor: Double): Option[ImageView] =
-    loadImage(path).map(myImage => new ImageView(myImage) {
-      fitWidth = myImage.width.value * factor
-      fitHeight = myImage.height.value * factor
-      preserveRatio = true
-    })
+    loadImage(path).map: image =>
+      createScaledImageView(image, factor)
 
   def createImageView(imagePath: String, width: Int): Either[String, ImageView] =
-    for
-      image <- loadImage(imagePath).toRight(s"Error to load image at path: $imagePath")
-    yield new ImageView(image) {
-      fitWidth = width
-      preserveRatio = true
-    }
-
-  private def loadImage(path: String): Option[Image] =
-    imageCache.get(path) match
-      case Some(cachedImage) => Some(cachedImage)
-      case None =>
-        Option(getClass.getResourceAsStream(path)).map { stream =>
-          val image = new Image(stream)
-          imageCache.update(path, image)
-          image
-        }
+    loadImage(imagePath)
+      .toRight(s"Error loading image at path: $imagePath")
+      .map: image =>
+        createFixedWidthImageView(image, width)
 
   def clearCache(): Unit =
     imageCache.clear()
-}
+    
+  private def loadImage(path: String): Option[Image] =
+    imageCache.get(path).orElse(loadAndCacheImage(path))
+    
+  private def loadAndCacheImage(path: String): Option[Image] =
+    Option(getClass.getResourceAsStream(path))
+      .map(createImageFromStream)
+      .map(cacheImage(path))
+      
+  private def createImageFromStream(stream: InputStream): Image =
+    new Image(stream)
+    
+  private def cacheImage(path: String)(image: Image): Image =
+    imageCache.update(path, image)
+    image
+    
+  private def createScaledImageView(myImage: Image, factor: Double): ImageView =
+    new ImageView(myImage):
+      fitWidth = myImage.width.value * factor
+      fitHeight = myImage.height.value * factor
+      preserveRatio = true
+      
+  private def createFixedWidthImageView(image: Image, width: Int): ImageView =
+    new ImageView(image):
+      fitWidth = width
+      preserveRatio = true
+      
+  object CacheStats:
+    def size: Int = imageCache.size
+
+    def contains(path: String): Boolean = imageCache.contains(path)
+
+    def cachedPaths: Seq[String] = imageCache.keys.toSeq
+
+    def memoryEstimate: String =
+      val totalPixels = imageCache.values.map: img =>
+        (img.width.value * img.height.value).toLong
+      .sum
+
+      val mbEstimate = (totalPixels * 4) / (1024 * 1024) // (4 bytes/pixel)
+      s"~${mbEstimate}MB"
