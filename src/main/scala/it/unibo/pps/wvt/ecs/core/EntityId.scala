d377393ba04014b304ab9f7f8de0ce097c8f134d
Giovanni Rinchiuso <giovanni.rinchiuso@studio.unibo.it>
2025-10-11 12:39:40 +0200

    [MODEL] Refactored Entity to avoid state conflicts

--- src/main/scala/it/unibo/pps/wvt/ecs/core/EntityId.scala
+++ src/main/scala/it/unibo/pps/wvt/ecs/core/EntityId.scala
@@ -1,13 +1,45 @@
 package it.unibo.pps.wvt.ecs.core
 
-case class EntityId(id: Long) extends AnyVal {
-  override def toString: String = s"EntityId($id)"
-}
+import java.util.concurrent.atomic.AtomicReference
+import scala.annotation.tailrec
 
-object EntityId {
-  private var counter: Long = 0L
+opaque type EntityId = Long
 
+object EntityId:
+  def apply(id: Long): EntityId = id
+
+  def generate(): EntityId = EntityIdGenerator.generate()
+
+  extension (id: EntityId)
+    def value: Long = id
+    def toString: String = s"EntityId($id)"
+
+case class EntityIdGenerator(nextId: Long = 1L):
+  def generate(): (EntityIdGenerator, EntityId) =
+    (EntityIdGenerator(nextId + 1), EntityId(nextId))
+
+  def generateN(n: Int): (EntityIdGenerator, List[EntityId]) =
+    (1 to n).foldLeft((this, List.empty[EntityId])): (acc, _) =>
+      val (gen, ids) = acc
+      val (newGen, newId) = gen.generate()
+      (newGen, ids :+ newId)
+
+object EntityIdGenerator:
+  private val globalGenerator: AtomicReference[EntityIdGenerator] =
+    AtomicReference(EntityIdGenerator())
+
+  @tailrec
   def generate(): EntityId =
-    counter += 1
-    EntityId(counter)
-}
+    val currentGen = globalGenerator.get()
+    val (newGen, newId) = currentGen.generate()
+
+    if globalGenerator.compareAndSet(currentGen, newGen) then
+      newId
+    else
+      generate()
+
+  def reset(): Unit =
+    globalGenerator.set(EntityIdGenerator())
+
+  def current: EntityIdGenerator =
+    globalGenerator.get()
