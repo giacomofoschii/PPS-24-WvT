9e289cc719d3374440fd31c8324ed0a75878e1c3
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-10-13 23:27:52 +0200

    [SPRINT4][REFACTOR] World refactored for immutability

--- src/main/scala/it/unibo/pps/wvt/ecs/core/World.scala
+++ src/main/scala/it/unibo/pps/wvt/ecs/core/World.scala
@@ -1,100 +1,101 @@
 package it.unibo.pps.wvt.ecs.core
 
-import it.unibo.pps.wvt.ecs.components.*
-import it.unibo.pps.wvt.utilities.*
+import it.unibo.pps.wvt.ecs.components._
+import it.unibo.pps.wvt.utilities.{GridMapper, Position}
 
 import scala.annotation.tailrec
-import scala.collection.mutable
-import scala.collection.mutable.Buffer
-import scala.reflect.ClassTag
 
-class World:
-  private val entities = mutable.Buffer[EntityId]()
-  private val components = mutable.Map[Class[_], mutable.Map[EntityId, Component]]()
-  private val entitiesByType = mutable.Map[String, mutable.Buffer[EntityId]]()
+case class World(
+                private val entities: Set[EntityId] = Set.empty,
+                private val components: Map[Class[_], Map[EntityId, Component]] = Map.empty,
+                private val entitiesByType: Map[String, Set[EntityId]] = Map.empty
+                ):
 
-  def createEntity(): EntityId =
+  def createentity(): (World, EntityId) =
     val entity = EntityId.generate()
-    entities.append(entity)
-    entity
+    (copy(entities = entities + entity), entity)
 
-  def destroyEntity(entity: EntityId): Unit =
-    entities --= Seq(entity)
-    components.values.foreach(_.remove(entity))
-    entitiesByType.values.foreach(_.--=(Seq(entity)))
+  def destroyEntity(entity: EntityId): World =
+    copy(
+      entities = entities - entity,
+      components = components.map((cmp, entityMap) => cmp -> (entityMap - entity)),
+      entitiesByType = entitiesByType.map((etype, entitySet) => etype -> (entitySet - entity))
+    )
 
-  def addComponent[T <: Component](entity: EntityId, component: T): Unit =
-    if(!entities.contains(entity)) throw new IllegalArgumentException(s"Entity $entity does not exist.")
-    val store = components.getOrElseUpdate(component.getClass, mutable.Map[EntityId, Component]())
-    store(entity) = component
+  def addComponent[T <: Component](entity: EntityId, component: T): World =
+    if !entities.contains(entity) then
+      this
+    else
+      val componentClass = component.getClass
+      val updatedComponents = components.updatedWith(componentClass): opt =>
+        Some(opt.getOrElse(Map.empty) + (entity -> component))
 
-    component match
-      case _ : WizardTypeComponent =>
-        entitiesByType.getOrElseUpdate("wizard", mutable.Buffer()).append(entity)
-      case _ : TrollTypeComponent =>
-        entitiesByType.getOrElseUpdate("troll", mutable.Buffer()).append(entity)
-      case _ : ProjectileTypeComponent =>
-        entitiesByType.getOrElseUpdate("projectile", mutable.Buffer()).append(entity)
-      case _ : Component =>
+      val updatedEntitiesByType = updateEntityTypeMapping(entity, component)
 
-  def removeComponent[T <: Component: ClassTag](entityId: EntityId): Unit =
-    components.get(implicitly[ClassTag[T]].runtimeClass).foreach(_.remove(entityId))
+      copy(components = updatedComponents, entitiesByType = updatedEntitiesByType)
 
-  def getComponent[T <: Component: ClassTag](entity: EntityId): Option[T] =
-    components.get(implicitly[ClassTag[T]].runtimeClass).flatMap(_.get(entity).map(_.asInstanceOf[T]))
+  def removeComponent[T <: Component](entityId: EntityId)(using ct: reflect.ClassTag[T]): World =
+    val componentClass = ct.runtimeClass
+    val updatedComponents = components.updatedWith(componentClass):
+      case Some(entityMap) =>
+        val newEntityMap = entityMap - entityId
+        if newEntityMap.isEmpty then None else Some(newEntityMap)
+      case None => None
 
-  def updateComponent[T <: Component: ClassTag](entity: EntityId, updateFn: T => T): Unit =
-    val compClass = implicitly[ClassTag[T]].runtimeClass
-    components.get(compClass).flatMap(_.get(entity)) match
+    copy(components = updatedComponents)
+
+  def updateComponent[T <: Component](entity: EntityId, updateFn: T => T)(using ct: reflect.ClassTag[T]): World =
+    val componentClass = ct.runtimeClass
+    getComponent[T](entity) match
       case Some(component) =>
-        val updatedComponent = updateFn(component.asInstanceOf[T])
-        removeComponent[T](entity)  
-        addComponent(entity, updatedComponent)  
+        val updatedComponent = updateFn(component)
+        removeComponent[T](entity).addComponent(entity, updatedComponent)
       case None =>
-        throw new NoSuchElementException(
-          s"Entity $entity does not have component of type ${compClass.getSimpleName}"
-        )
+        this
 
-  def hasComponent[T <: Component: ClassTag](entity: EntityId): Boolean =
+  def getComponent[T <: Component](entity: EntityId)(using ct: reflect.ClassTag[T]): Option[T] =
+    components.get(ct.runtimeClass).flatMap(_.get(entity).map(_.asInstanceOf[T]))
+
+  def hasComponent[T <: Component](entity: EntityId)(using ct: reflect.ClassTag[T]): Boolean =
     getComponent[T](entity).isDefined
 
-  def getEntitiesWithComponent[T <: Component: ClassTag]: mutable.Buffer[EntityId] =
-    components.get(implicitly[ClassTag[T]].runtimeClass).map(_.keys.toBuffer).getOrElse(mutable.Buffer.empty)
+  def getEntitiesWithComponent[T <: Component](using ct: reflect.ClassTag[T]): Set[EntityId] =
+    components.get(ct.runtimeClass).map(_.keySet).getOrElse(Set.empty)
 
-  def getEntitiesWithTwoComponents[A <: Component: ClassTag, B <: Component: ClassTag]: mutable.Buffer[EntityId] =
-    val compAEntities = getEntitiesWithComponent[A]
-    val compBEntities = getEntitiesWithComponent[B]
-    compAEntities.intersect(compBEntities)
+  def getEntititesWithTwoComponents[A <: Component, B <: Component]
+  (using ctA: reflect.ClassTag[A], ctB: reflect.ClassTag[B]): Set[EntityId] =
+    val entitiesA = getEntitiesWithComponent[A]
+    val entitiesB = getEntitiesWithComponent[B]
+    entitiesA.intersect(entitiesB)
 
-  def getEntitiesByType(entityType: String): mutable.Buffer[EntityId] =
-    entitiesByType.getOrElse(entityType, mutable.Buffer.empty)
+  def getEntitiesByType(entityType: String): Set[EntityId] =
+    entitiesByType.getOrElse(entityType, Set.empty)
 
-  def getAllEntities: mutable.Buffer[EntityId] = entities
+  def getAllEntities: Set[EntityId] = entities
 
   def getEntityAt(position: Position): Option[EntityId] =
-    GridMapper.physicalToLogical(position) match
-      case None => None
-      case Some(targetGrid) =>
-        @tailrec
-        def findEntityRecursive(remaining: List[EntityId]): Option[EntityId] =
-          remaining match
-            case Nil => None
-            case head :: tail =>
-              getComponent[PositionComponent](head) match
-                case Some(posComp) =>
-                  GridMapper.physicalToLogical(posComp.position) match
-                    case Some(entityGrid) if entityGrid == targetGrid =>
-                      Some(head)
-                    case _ => findEntityRecursive(tail)
-                case _ => findEntityRecursive(tail)
+    GridMapper.physicalToLogical(position).flatMap: targetGrid =>
 
-        val entitiesWithPos = getEntitiesWithComponent[PositionComponent].toList
-        findEntityRecursive(entitiesWithPos)
-        
+      @tailrec
+      def findEntityRecursive(remaining: List[EntityId]): Option[EntityId] =
+        remaining match
+          case Nil => None
+          case head :: tail =>
+            getComponent[PositionComponent](head) match
+              case Some(posComp) =>
+                GridMapper.physicalToLogical(posComp.position) match
+                  case Some(entityGrid) if entityGrid == targetGrid =>
+                    Some(head)
+                  case _ => findEntityRecursive(tail)
+              case _ => findEntityRecursive(tail)
+
+      val entitiesList = getEntitiesWithComponent[PositionComponent].toList
+      findEntityRecursive(entitiesList)
+
   private def getEntitiesAt(position: Position): Seq[EntityId] =
     for
       targetGrid <- GridMapper.physicalToLogical(position).toSeq
-      entity <- getEntitiesWithComponent[PositionComponent]
+      entity <- getEntitiesWithComponent[PositionComponent].toSeq
       posComp <- getComponent[PositionComponent](entity)
       entityGrid <- GridMapper.physicalToLogical(posComp.position)
       if entityGrid == targetGrid
@@ -108,7 +109,29 @@ class World:
       .orElse(getComponent[TrollTypeComponent](entity))
       .orElse(getComponent[ProjectileTypeComponent](entity))
 
-  def clear(): Unit =
-    entities.clear()
-    components.clear()
-    entitiesByType.clear()
+  def clear(): World =
+    World()
+
+  def size: Int = entities.size
+
+  def isEmpty: Boolean = entities.isEmpty
+
+  private def updateEntityTypeMapping(entity: EntityId, component: Component): Map[String, Set[EntityId]] =
+    component match
+      case _: WizardTypeComponent =>
+        entitiesByType.updatedWith("Wizard"): opt =>
+          Some(opt.getOrElse(Set.empty) + entity)
+      case _: TrollTypeComponent =>
+        entitiesByType.updatedWith("Troll"): opt =>
+          Some(opt.getOrElse(Set.empty) + entity)
+      case _: ProjectileTypeComponent =>
+        entitiesByType.updatedWith("Projectile"): opt =>
+          Some(opt.getOrElse(Set.empty) + entity)
+      case _ => entitiesByType
+
+object World:
+
+  def empty: World = World()
+
+  def withEntities(entities: EntityId*): World =
+    World(entities = entities.toSet)
