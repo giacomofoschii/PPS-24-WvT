34be85a89f80bd0624ea119f9e2adb5af7c6955c
Giacomo Foschi <giacomo.foschi3@studio.unibo.it>
2025-10-15 16:23:52 +0200

    Refactor CollisionSystemTest to use AnyFlatSpec and improve test readability; add RenderSystemTest and CombatSystemTest for enhanced coverage

--- src/test/scala/it/unibo/pps/wvt/ecs/systems/CollisionSystemTest.scala
+++ src/test/scala/it/unibo/pps/wvt/ecs/systems/CollisionSystemTest.scala
@@ -3,107 +3,85 @@ package it.unibo.pps.wvt.ecs.systems
 import it.unibo.pps.wvt.ecs.components.*
 import it.unibo.pps.wvt.ecs.core.{EntityId, World}
 import it.unibo.pps.wvt.utilities.{GridMapper, Position}
-
-import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.BeforeAndAfter
 
-class CollisionSystemTest extends AnyFunSuite with Matchers with BeforeAndAfter:
-  var world: World                     = _
+class CollisionSystemTest extends AnyFlatSpec with Matchers with BeforeAndAfter:
+
+  var world: World = _
   var collisionSystem: CollisionSystem = _
 
   before:
-    world = new World()
+    world = World()
     collisionSystem = CollisionSystem()
 
-  test("should detect collision between projectile and troll"):
-    val world  = World()
-    val system = CollisionSystem()
+  behavior of "CollisionSystem"
 
+  it should "detect collision between a projectile and a troll in the same cell" in:
     val projectilePos = GridMapper.logicalToPhysical((2, 2)).get
-    val projectile    = world.createEntity()
-    world.addComponent(projectile, ProjectileTypeComponent(ProjectileType.Wind))
-    world.addComponent(projectile, PositionComponent(projectilePos))
-    world.addComponent(projectile, DamageComponent(25, ProjectileType.Wind))
+    val (world1, projectile) = world.createEntity()
+    var w = world1.addComponent(projectile, ProjectileTypeComponent(ProjectileType.Wind))
+      .addComponent(projectile, PositionComponent(projectilePos))
+      .addComponent(projectile, DamageComponent(25, ProjectileType.Wind))
 
-    val troll = world.createEntity()
-    world.addComponent(troll, TrollTypeComponent(TrollType.Base))
-    world.addComponent(troll, PositionComponent(projectilePos))
-    world.addComponent(troll, HealthComponent(100, 100))
+    val (world2, troll) = w.createEntity()
+    w = world2.addComponent(troll, TrollTypeComponent(TrollType.Base))
+      .addComponent(troll, PositionComponent(projectilePos))
+      .addComponent(troll, HealthComponent(100, 100))
 
-    system.update(world)
+    val (finalWorld, _) = collisionSystem.update(w)
 
-    world.getAllEntities should not contain projectile
-    val trollHealth = world.getComponent[HealthComponent](troll)
+    finalWorld.getAllEntities should not contain projectile
+    val trollHealth = finalWorld.getComponent[HealthComponent](troll)
     trollHealth.get.currentHealth should be < 100
 
-  test("should not detect collision in different cells"):
-    val world  = World()
-    val system = CollisionSystem()
+  it should "not detect collision when entities are in different cells" in:
+    val (world1, projectile) = world.createEntity()
+    var w = world1.addComponent(projectile, ProjectileTypeComponent(ProjectileType.Wind))
+      .addComponent(projectile, PositionComponent(GridMapper.logicalToPhysical((0, 0)).get))
+      .addComponent(projectile, DamageComponent(25, ProjectileType.Wind))
 
-    val projectile = world.createEntity()
-    world.addComponent(projectile, ProjectileTypeComponent(ProjectileType.Wind))
-    world.addComponent(projectile, PositionComponent(GridMapper.logicalToPhysical((0, 0)).get))
-    world.addComponent(projectile, DamageComponent(25, ProjectileType.Wind))
+    val (world2, troll) = w.createEntity()
+    w = world2.addComponent(troll, TrollTypeComponent(TrollType.Base))
+      .addComponent(troll, PositionComponent(GridMapper.logicalToPhysical((1, 1)).get))
+      .addComponent(troll, HealthComponent(100, 100))
 
-    val troll = world.createEntity()
-    world.addComponent(troll, TrollTypeComponent(TrollType.Base))
-    world.addComponent(troll, PositionComponent(GridMapper.logicalToPhysical((5, 5)).get))
-    world.addComponent(troll, HealthComponent(100, 100))
+    val (finalWorld, _) = collisionSystem.update(w)
 
-    system.update(world)
+    finalWorld.getAllEntities should contain(projectile)
+    finalWorld.getComponent[HealthComponent](troll).get.currentHealth shouldBe 100
 
-    world.getAllEntities should contain(projectile)
-    world.getAllEntities should contain(troll)
+  it should "process melee collision between a troll and a wizard" in:
+    val pos = GridMapper.logicalToPhysical((2, 2)).get
+    val (world1, troll) = world.createEntity()
+    var w = world1.addComponent(troll, TrollTypeComponent(TrollType.Base))
+      .addComponent(troll, PositionComponent(pos))
+      .addComponent(troll, AttackComponent(20, 1.0, 1000))
 
-  test("should process melee collision between troll and wizard"):
-    val world  = World()
-    val system = CollisionSystem()
+    val (world2, wizard) = w.createEntity()
+    w = world2.addComponent(wizard, WizardTypeComponent(WizardType.Fire))
+      .addComponent(wizard, PositionComponent(pos))
+      .addComponent(wizard, HealthComponent(100, 100))
 
-    val pos   = GridMapper.logicalToPhysical((2, 2)).get
-    val troll = world.createEntity()
-    world.addComponent(troll, TrollTypeComponent(TrollType.Base))
-    world.addComponent(troll, PositionComponent(pos))
-    world.addComponent(troll, AttackComponent(20, 1.0, 1000))
+    val (finalWorld, _) = collisionSystem.update(w)
 
-    val wizard = world.createEntity()
-    world.addComponent(wizard, WizardTypeComponent(WizardType.Fire))
-    world.addComponent(wizard, PositionComponent(pos))
-    world.addComponent(wizard, HealthComponent(100, 100))
+    finalWorld.getComponent[CollisionComponent](wizard) shouldBe defined
+    finalWorld.getComponent[BlockedComponent](troll) shouldBe defined
 
-    system.update(world)
+  it should "apply ice freeze effect on collision" in:
+    val pos = GridMapper.logicalToPhysical((3, 3)).get
+    val (world1, projectile) = world.createEntity()
+    var w = world1.addComponent(projectile, ProjectileTypeComponent(ProjectileType.Ice))
+      .addComponent(projectile, PositionComponent(pos))
+      .addComponent(projectile, DamageComponent(10, ProjectileType.Ice))
 
-    world.getComponent[CollisionComponent](wizard) shouldBe defined
-    world.getComponent[BlockedComponent](troll) shouldBe defined
+    val (world2, troll) = w.createEntity()
+    w = world2.addComponent(troll, TrollTypeComponent(TrollType.Base))
+      .addComponent(troll, PositionComponent(pos))
+      .addComponent(troll, HealthComponent(100, 100))
 
-  test("should apply ice freeze effect"):
-    val world  = World()
-    val system = CollisionSystem()
+    val (finalWorld, _) = collisionSystem.update(w)
 
-    val pos        = GridMapper.logicalToPhysical((3, 3)).get
-    val projectile = world.createEntity()
-    world.addComponent(projectile, ProjectileTypeComponent(ProjectileType.Ice))
-    world.addComponent(projectile, PositionComponent(pos))
-    world.addComponent(projectile, DamageComponent(25, ProjectileType.Ice))
-
-    val troll = world.createEntity()
-    world.addComponent(troll, TrollTypeComponent(TrollType.Base))
-    world.addComponent(troll, PositionComponent(pos))
-    world.addComponent(troll, HealthComponent(100, 100))
-
-    system.update(world)
-
-    world.getComponent[FreezedComponent](troll) shouldBe defined
-
-  test("should remove BlockedComponent when blocker is gone"):
-    val world  = World()
-    val system = CollisionSystem()
-
-    val troll = world.createEntity()
-    world.addComponent(troll, TrollTypeComponent(TrollType.Base))
-    world.addComponent(troll, PositionComponent(Position(100, 100)))
-    world.addComponent(troll, BlockedComponent(EntityId(999)))
-
-    system.update(world)
-
-    world.getComponent[BlockedComponent](troll) shouldBe None
+    finalWorld.getComponent[FreezedComponent](troll) shouldBe defined
+    finalWorld.getComponent[FreezedComponent](troll).get.speedModifier shouldBe 0.5
